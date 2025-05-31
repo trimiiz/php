4a53335a7dde18ca4b65b3241588bffd64fd1d56
trimiiz <168911053+trimiiz@users.noreply.github.com>
2025-05-31 14:45:26 +0200

    Add files via upload

---
+++ php/php1/main.php
@@ -0,0 +1,17 @@
+<?php
+$school="Digital School";
+$student_id=1;
+$number=1.5;
+$booleans=false;
+$array=['Studenti1','Studenti2','Studenti3'];
+
+$x=8;
+$y=3;
+
+echo "I love $school";
+echo $x+$y;
+
+?>
+
+
+
